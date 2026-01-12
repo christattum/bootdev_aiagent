144da0cebc71414e068fd0f8f6b95c0da2fd77c8
Chris Tattum <chris.tattum@gmail.com>
2026-01-12 17:43:12 +0000

    add command line arg for prompt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import Client
@@ -9,12 +10,16 @@ api_key = os.environ.get("GEMINI_API_KEY")
 if api_key == None:
     raise Exception("API key not set")
 
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+# Now we can access `args.user_prompt`
 
 client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    contents=args.user_prompt
 )
 
 if response.usage_metadata != None:
