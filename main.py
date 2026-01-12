25cb91c17ed3dea1c7f4c0d95954c43d060a1507
Chris Tattum <chris.tattum@gmail.com>
2026-01-12 16:43:32 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+from google.genai import Client
+
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+if api_key == None:
+    raise Exception("API key not set")
+
+
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+)
+
+print(response.text)
