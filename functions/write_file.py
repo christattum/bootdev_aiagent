3121bc99f6efe91c1ec3a82404b575ae6bd98e9e
Chris Tattum <chris.tattum@gmail.com>
2026-01-16 19:30:20 +0000

    fix write_file

--- functions/write_file.py
+++ functions/write_file.py
@@ -10,25 +10,25 @@ def write_file(working_directory, file_path, content):
         if os.path.isdir(target_file):
             return f'Error: Cannot write to "{file_path}" as it is a directory'
 
-        # check target file is a valid file
-        if not os.path.isfile(target_file):
-            return f'Error: File not found or is not a regular file: "{file_path}"'
-
         # Check target file is within working directory
         valid_target_file = os.path.commonpath([abs_working_dir, target_file]) == abs_working_dir
         if not valid_target_file:
             return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
         
         # Create any required parent directories
-        os.makedirs(valid_target_file, exist_ok = True)
+        dir_path = os.path.dirname(target_file)
+        os.makedirs(dir_path, exist_ok = True)
 
         # update file contents
-        f = open(file_path, "w")
+        f = open(target_file, "w")
         chars = f.write(content)
         if chars != len(content):
-            return f"Error: Wrote {chars} to {file_path} but content was {len(content)}"        
+            return f"Error: Wrote {chars} to {file_path} but content was {len(content)} bytes"        
             
         return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
     
     except Exception as e:
         return f'Error: {e}'
+
+
+
