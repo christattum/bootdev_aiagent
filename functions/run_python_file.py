bd09db0a19aeeaab7259431fd81baf98d10f78ba
Chris Tattum <chris.tattum@gmail.com>
2026-01-17 18:44:01 +0000

    add validations

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,2 +1,19 @@
+import os
+
 def run_python_file(working_directory, file_path, args=None):
-    pass
+    
+    abs_working_dir = os.path.abspath(working_directory)
+    target_file = os.path.normpath(os.path.join(abs_working_dir, file_path))
+
+    # Check file_path ends in .py
+    if len(target_file) < 3 or target_file[-3:] != ".py":
+        return f'Error: "{file_path}" is not a Python file'
+
+    # Check file_path is a file
+    if not os.path.isfile(target_file):
+        return f'Error: "{file_path}" does not exist or is not a regular file'
+
+    # Check target file is within working directory
+    if not os.path.commonpath([abs_working_dir, target_file]) == abs_working_dir:
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+        
