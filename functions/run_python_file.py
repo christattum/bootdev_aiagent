0229506cdecf397cb18537d0c6e53a91b1970524
Chris Tattum <chris.tattum@gmail.com>
2026-01-19 07:12:04 +0000

    complete run_python_file and tests

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -2,27 +2,46 @@ import os
 import subprocess
 
 def run_python_file(working_directory, file_path, args=None):
+
+    try:
     
-    abs_working_dir = os.path.abspath(working_directory)
-    target_file = os.path.normpath(os.path.join(abs_working_dir, file_path))
+        abs_working_dir = os.path.abspath(working_directory)
+        target_file = os.path.normpath(os.path.join(abs_working_dir, file_path))
 
-    # Check file_path ends in .py
-    if len(target_file) < 3 or target_file[-3:] != ".py":
-        return f'Error: "{file_path}" is not a Python file'
+        # Check file_path ends in .py
+        if len(target_file) < 3 or target_file[-3:] != ".py":
+            return f'Error: "{file_path}" is not a Python file'
 
-    # Check file_path is a file
-    if not os.path.isfile(target_file):
-        return f'Error: "{file_path}" does not exist or is not a regular file'
+        # Check file_path is a file
+        if not os.path.isfile(target_file):
+            return f'Error: "{file_path}" does not exist or is not a regular file'
 
-    # Check target file is within working directory
-    if not os.path.commonpath([abs_working_dir, target_file]) == abs_working_dir:
-        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+        # Check target file is within working directory
+        if not os.path.commonpath([abs_working_dir, target_file]) == abs_working_dir:
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+            
+        # Run the file
+        command = ["python", target_file]
+        command.extend(args)
+        process = subprocess.run(command,
+                                cwd=abs_working_dir, 
+                                capture_output=True, 
+                                text=True, 
+                                timeout=30)
+
+        if process.returncode:
+            return f"Process exited with code {process.returncode}"
         
-    # Run the file
-    command = ["python", target_file]
-    command.extend(args)
-    process = subprocess.run(command,
-                             cwd=abs_working_dir, 
-                             capture_output=True, 
-                             text=True, 
-                             timeout=30)
+        result = []
+        if process.stderr:
+            result.append(f"STDERR: {process.stderr}")
+        if process.stdout:
+            result.append(f"STDOUT: {process.stdout}")
+
+        if result:
+            return "\n".join(result)
+        else:
+            return "No output produced"
+        
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
